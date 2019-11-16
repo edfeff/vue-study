53b4b29194112fd3c4b6b220f779d188f428157b
edfeff <396859442@qq.com>
2019-11-16 08:38:22 +0800

    init

---
+++ c1/cli/src/components/Hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p v-if="isMorning">早上好</p>
+    <p v-if="isAfterNoon">中午好</p>
+    <p v-if="isEvening">晚上好</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMorning: new Date().getHours() < 12,
+      isAfterNoon: new Date().getHours() >= 12 && new Date().getHours() < 18,
+      isEvening: new Date().getHours() >= 18
+    };
+  }
+};
+</script>
+
+<style></style>
